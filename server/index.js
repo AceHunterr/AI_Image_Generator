0bedbc24fb61885d5878827b9f38e96fbc48f8d8
AceHunterr <aggarwalmehul26@gmail.com>
2023-06-28 03:33:45 +0530

    Started server

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
 
+app.get('/', async(req,res)=>{
+    res.send('Hello from DALL-E'); 
+}) 
+
+const startServer = async()=>{
+    app.listen(8080,()=>console.log('Server has started on port 8080...'))
+}
+
+startServer();
+
+
